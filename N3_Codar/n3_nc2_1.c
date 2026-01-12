eff59d25067db7749f88833f00cc58a198e9f539
eltonfreitasr-facl <elton.freitasr@gmail.com>
2026-01-12 21:40:44 +0000

    N3_Codar

---
+++ N3_Codar/n3_nc2_1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int a = 10;
+    int b = 20;
+
+/*
+    > (maior que)
+    < (menor que)
+    >= (maior ou igual a)
+    <= (menor ou igual a)
+    == (igual a)
+    != (diferente de)
+*/
+
+
+    printf("a > b: %d\n", a > b);     // a maior que b
+    printf("a < b: %d\n", a < b);     // a menor que b
+    printf("a == b: %d\n", a == b);   // a igual a b
+    printf("a != b: %d\n", a != b);   // a diferente de b
+
+    return 0;
+
+
+}
